7def8e10aae9d85cc9048dcae695eac1933f21b5
Mohamed-4rarh <mainmohedadhame@gmail.com>
2025-02-19 21:46:26 +0200

    dashboard folder structure

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 import { Home } from "lucide-react";
 import Link from "next/link";
 
-export default async function DashboardPage() {
+export default function DashboardPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100 dark:bg-gray-900">
       <p className="text-gray-600 text-3xl mb-10">
